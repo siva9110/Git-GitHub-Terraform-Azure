56a03534a4982d60448845c30860aa6bfe376c47
SIVA PRASAD <golisiva50@gmail.com>
2025-11-25 12:32:28 +0530

    updated!!

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "user_principal_name" {
-    value = azuread_user.Terraform.user_principal_name   
-}
+# output "user_principal_name" {
+#     value = azuread_user.Terraform.user_principal_name  
+# }
 
-output "user_object_id" {
+# output "user_object_id" {
 
-  value = azuread_user.Terraform.object_id
+#   value = azuread_user.Terraform.object_id
   
-}
+# }
 
 output "user_principal_name" {
     value = azuread_user.Terraform-1.user_principal_name   
