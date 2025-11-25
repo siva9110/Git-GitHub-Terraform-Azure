991e3133f5e7bbbe332eb4836cd321dfcd23e86c
SIVA PRASAD <golisiva50@gmail.com>
2025-11-25 12:00:50 +0530

    Updated the Provider.tf 2nd time

--- Provider.tf
+++ Provider.tf
@@ -1,50 +1,50 @@
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-    msgraph = {
-      source  = "microsoftgraph/msgraph"
-      version = "~> 0.2.0" # Check latest version
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 4.0"
+#     }
+#     msgraph = {
+#       source  = "microsoftgraph/msgraph"
+#       version = "~> 0.2.0" # Check latest version
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
-provider "msgraph" {
-  tenant_id     = var.tenant_id
-  client_id     = var.client_id
-  client_secret = var.client_secret
-}
+# provider "msgraph" {
+#   tenant_id     = var.tenant_id
+#   client_id     = var.client_id
+#   client_secret = var.client_secret
+# }
 
 
 
 
 
 ###############################################################################################
-# terraform {
-#   required_providers {
-#     azuread = {
-#       source  = "hashicorp/azuread"
-#       version = "~> 2.47.0" # Use latest stable
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47.0" # Use latest stable
+    }
+  }
+}
 
-# provider "azuread" {
+provider "azuread" {
+  tenant_id     = var.tenant_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
+}
+
+# provider "msgraph" {
 #   tenant_id     = var.tenant_id
 #   client_id     = var.client_id
 #   client_secret = var.client_secret
 # }
-
-# # provider "msgraph" {
-# #   tenant_id     = var.tenant_id
-# #   client_id     = var.client_id
-# #   client_secret = var.client_secret
-# # }
 
