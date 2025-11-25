3e6555e0d3180826fcc95d3b8a72ab0033a5ce1d
SIVA PRASAD <golisiva50@gmail.com>
2025-11-25 12:24:23 +0530

    just test !!!

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
 output "user_principal_name" {
-    value = azuread_user.example.user_principal_name
-   
+    value = azuread_user.Terraform.user_principal_name   
 }
 
 output "user_object_id" {
 
-  value = azuread_user.example.object_id
+  value = azuread_user.Terraform.object_id
   
 }
--- users.tf
+++ users.tf
@@ -1,4 +1,4 @@
-resource "azuread_user" "example" {
+resource "azuread_user" "Terraform" {
   user_principal_name = "Git-GitHub-Terraform-Azure@76hpc3.onmicrosoft.com"
   display_name        = "Git-GitHub-Terraform-Azure"
   password            = "ComplexPassword123!"
