916dceff7406986f007f1eb2846f2a1bf9073ac4
SIVA PRASAD <golisiva50@gmail.com>
2025-11-26 13:22:29 +0530

    out

--- outputs.tf
+++ outputs.tf
@@ -29,20 +29,39 @@ output "user_object_id_user7" {
   value = azuread_user.user7.object_id
   
 }
-output "user_principal_name_userhari" {
-    value = azuread_user.userhari.user_principal_name   
+
+<<<<<<< HEAD
+output "user_principal_name_usersiva" {
+    value = azuread_user.usersiva.user_principal_name   
 }
 
-output "user_object_id_userhari" {
-  value = azuread_user.userhari.object_id
+output "user_object_id_usersiva" {
+
+=======
+<<<<<<< HEAD
+
+output "user_principal_name_user7" {
+    value = azuread_user.usersiva.user_principal_name   
+}
+
+output "user_object_id_user7" {
+=======
+output "user_principal_name_usersiva" {
+    value = azuread_user.usersiva.user_principal_name   
+}
+
+output "user_object_id_usersiva" {
+>>>>>>> users
+
+>>>>>>> 6392880bc04eb9bdf46cc3f4e5c10e1fca7cd215
+  value = azuread_user.usersiva.object_id
   
 }
+<<<<<<< HEAD
+=======
 
-output "user_principal_name_userhari1" {
-    value = azuread_user.userhari1.user_principal_name   
-}
 
-output "user_object_id_userhari1" {
-  value = azuread_user.userhari1.object_id
-  
-}
+<<<<<<< HEAD
+=======
+>>>>>>> users
+>>>>>>> 6392880bc04eb9bdf46cc3f4e5c10e1fca7cd215
