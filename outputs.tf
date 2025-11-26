2d827178077185c944492768400b45a2ef081bda
SIVA PRASAD <golisiva50@gmail.com>
2025-11-26 13:24:08 +0530

    test now

--- outputs.tf
+++ outputs.tf
@@ -29,39 +29,3 @@ output "user_object_id_user7" {
   value = azuread_user.user7.object_id
   
 }
-
-<<<<<<< HEAD
-output "user_principal_name_usersiva" {
-    value = azuread_user.usersiva.user_principal_name   
-}
-
-output "user_object_id_usersiva" {
-
-=======
-<<<<<<< HEAD
-
-output "user_principal_name_user7" {
-    value = azuread_user.usersiva.user_principal_name   
-}
-
-output "user_object_id_user7" {
-=======
-output "user_principal_name_usersiva" {
-    value = azuread_user.usersiva.user_principal_name   
-}
-
-output "user_object_id_usersiva" {
->>>>>>> users
-
->>>>>>> 6392880bc04eb9bdf46cc3f4e5c10e1fca7cd215
-  value = azuread_user.usersiva.object_id
-  
-}
-<<<<<<< HEAD
-=======
-
-
-<<<<<<< HEAD
-=======
->>>>>>> users
->>>>>>> 6392880bc04eb9bdf46cc3f4e5c10e1fca7cd215
